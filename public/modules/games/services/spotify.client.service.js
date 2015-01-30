e73f06c648a0ad73ae2fcba344986af6fcc8825a
Michael Bukosky <mbukosky@gmail.com>
2015-01-30 14:55:35 -0500

    hooked up basic Spotify top 20 list

--- public/modules/games/services/spotify.client.service.js
+++ public/modules/games/services/spotify.client.service.js
@@ -1,15 +1,40 @@
 'use strict';
 
-angular.module('games').factory('Spotify', [
-	function() {
-		// Spotify service logic
-		// ...
+angular.module('games').factory('Spotify', ['$http',
+    function($http) {
+        // Spotify service logic
+        // ...
 
-		// Public API
-		return {
-			someMethod: function() {
-				return true;
-			}
-		};
-	}
+        var query = 'https://api.spotify.com/v1/search?q=a&type=album&market=US&limit=20&offset=5';
+        var get_albums_query = 'https://api.spotify.com/v1/albums?ids=';
+
+        // Public API
+        return {
+            top20: function() {
+                var albums = [];
+                var ans = [];
+                $http.get(query).success(function(data, status) {
+                    angular.forEach(data.albums.items, function(value, key) {
+                        albums.push(value.id);
+                    });
+
+                    var a_q = get_albums_query + albums.join();
+                    $http.get(a_q).success(function(data, status) {
+
+                        angular.forEach(data.albums, function(value, key) {
+
+                            ans.push({
+                                albumName: value.name,
+                                artistsName: value.artists[0].name
+                            });
+
+                        });
+
+                    });
+                });
+
+                return ans;
+            }
+        };
+    }
 ]);
