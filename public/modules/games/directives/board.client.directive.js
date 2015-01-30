19a1ac99ce2d11eb9a55ddeb5ecc0e43c04ff848
Curt Layton <laytonc@hotmail.com>
2015-01-30 13:01:51 -0500

    UI stubs

--- public/modules/games/directives/board.client.directive.js
+++ public/modules/games/directives/board.client.directive.js
@@ -1,1 +1,6 @@
-﻿
+﻿angular.module('games').directive('board', function () {
+    return {
+        restrict: "E",
+        template: '<h2>Round: <span class="label label-success">0</span></h2><div>question directive</div><div>list of player directives</div>'
+    }
+})
